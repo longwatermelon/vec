e94c2fd19b2257c8c07735d18cc1473bd6010cfc
longwatermelon <longwatermelon1@gmail.com>
2023-03-07 20:27:16 -0700

    first commit

---
+++ vec.hpp
@@ -0,0 +1,89 @@
+#pragma once
+#include <cstddef>
+#include <array>
+#include <cmath>
+#include <string>
+
+template <typename T, size_t N>
+struct Vec
+{
+    template <typename ...Args>
+    Vec(Args ...args)
+        : data{ args... } {}
+
+    Vec(const std::array<T, N> &data)
+        : data(data) {}
+
+    std::array<T, N> data;
+
+    T operator[](int i) const
+    {
+        return data[i];
+    }
+
+    Vec<T, N> operator+(const Vec<T, N> &other) const
+    {
+        std::array<T, N> arr;
+        for (size_t i = 0; i < N; ++i)
+            arr[i] = data[i] + other[i];
+        return Vec(arr);
+    }
+
+    Vec<T, N> operator-(const Vec<T, N> &other) const
+    {
+        return *this + -other;
+    }
+
+    Vec<T, N> operator-() const
+    {
+        std::array<T, N> arr;
+        for (size_t i = 0; i < N; ++i)
+            arr[i] = -data[i];
+        return Vec(arr);
+    }
+
+    Vec<T, N> operator*(T s) const
+    {
+        std::array<T, N> arr;
+        for (size_t i = 0; i < N; ++i)
+            arr[i] = data[i] * s;
+        return Vec(arr);
+    }
+
+    Vec<T, N> operator/(T s) const
+    {
+        std::array<T, N> arr;
+        for (size_t i = 0; i < N; ++i)
+            arr[i] = data[i] / s;
+        return Vec(arr);
+    }
+
+    T dot(const Vec<T, N> &other) const
+    {
+        T sum = 0;
+        for (size_t i = 0; i < N; ++i)
+            sum += data[i] * other[i];
+        return sum;
+    }
+
+    T length() const
+    {
+        T len = 0;
+        for (size_t i = 0; i < N; ++i)
+            len += data[i] * data[i];
+        return std::sqrt(len);
+    }
+
+    Vec<T, N> normalize() const
+    {
+        return *this / length();
+    }
+
+    std::string str() const
+    {
+        std::string s = "[";
+        for (size_t i = 0; i < N; ++i)
+            s += std::to_string(data[i]) + (i == N - 1 ? "]" : ", ");
+        return s;
+    }
+};
